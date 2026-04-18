565638c6938233c9d8ce33144eb168babfa5b3ad
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2026-04-18 14:30:20 +0200

    feat: sidebar

--- apps/frontend/src/routes/(app)/e/[environment_id]/+page.svelte
+++ apps/frontend/src/routes/(app)/e/[environment_id]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import EnvironmentsBar from "ui/shell/environments-bar/environments-bar.svelte";
+	import { EnvironmentsBar } from "ui/shell/environments-bar";
+	import { Sidebar } from "ui/shell/sidebar";
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col h-dvh">
 	<EnvironmentsBar />
+	<Sidebar />
 </div>
---
+++ apps/frontend/src/ui/shell/sidebar/root.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Button from "ui/primitives/button.svelte";
+	import Input from "ui/primitives/input.svelte";
+	import PhMagnifyingGlassDuotone from "~icons/ph/magnifying-glass-duotone";
+	import PhDotsThreeBold from "~icons/ph/dots-three-bold";
+</script>
+
+<aside class="w-60 border-r border-main-900 flex-1 flex flex-col">
+	<Input
+		id="search-bar"
+		name="search-bar"
+		variant="ghost"
+		class="border-b border-main-900 py-2.5 px-2 focus-visible:ring-0"
+		leftIconClass="text-main-600"
+		placeholder="Find..."
+		icons={{ left: PhMagnifyingGlassDuotone }}
+	/>
+
+	<nav class="flex-1">
+		<ul></ul>
+	</nav>
+
+	<div
+		class="h-11 flex items-center px-3 gap-x-2 border-t border-main-900 text-sm"
+	>
+		<img
+			class="size-6 rounded-full object-cover"
+			alt=""
+			src="https://i.pinimg.com/736x/0b/ed/9c/0bed9c847df1776b9c18fe7edb0ad0a2.jpg"
+		/>
+		okzmo
+
+		<Button
+			aria-label="settings"
+			variant="ghost"
+			class="size-5.5 border border-main-700 rounded-full ml-auto"
+		>
+			<PhDotsThreeBold class="size-4 text-main-300" />
+		</Button>
+	</div>
+</aside>
