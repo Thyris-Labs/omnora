70e7b69134e15780f2feb35ee0d386c505ec79fd
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2026-04-22 21:16:56 +0200

    refacto: remove sidebar, rename environments-bar to top-bar

---
+++ apps/frontend/src/features/shell/components/top-bar/index.ts
@@ -0,0 +1,1 @@
+export { default as TopBar } from "./root.svelte"
--- apps/frontend/src/routes/(app)/e/[environment_id]/+layout.svelte
+++ apps/frontend/src/routes/(app)/e/[environment_id]/+layout.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	import { EnvironmentsBar } from "features/shell/components/environments-bar";
-	import { Sidebar } from "features/shell/components/sidebar";
+	import { TopBar } from "features/shell/components/top-bar";
 
 	let { children } = $props();
 </script>
 
 <div class="flex flex-col h-dvh">
-	<EnvironmentsBar />
-	<div class="flex flex-1">
-		<Sidebar />
-		<main class="flex-1">
-			{@render children?.()}
-		</main>
-	</div>
+	<TopBar />
+	<main class="flex-1">
+		{@render children?.()}
+	</main>
 </div>
