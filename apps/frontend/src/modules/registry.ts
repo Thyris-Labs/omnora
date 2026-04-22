14a6f5c06e29c160d715650040ab4b62e8e1a5e2
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2026-04-22 21:56:23 +0200

    feat: ideas of modules

--- apps/frontend/src/modules/registry.ts
+++ apps/frontend/src/modules/registry.ts
@@ -2,7 +2,7 @@ import type { Component } from "svelte"
 import type { SVGAttributes } from "svelte/elements"
 import PhNotePencilDuotone from "~icons/ph/note-pencil-duotone"
 
-export type ModuleTypes = "NOTES"
+export type ModuleTypes = "NOTES" | "INBOX" | "GALLERY" | "VAULT" | "PASSWORDS" | "REPOSITORIES" | "CHAT"
 
 interface ModuleInformations {
 	name: string
@@ -10,5 +10,11 @@ interface ModuleInformations {
 }
 
 export const MODULES: Record<ModuleTypes, ModuleInformations> = {
-	NOTES: { name: "Notes", Icon: PhNotePencilDuotone }
+	NOTES: { name: "Notes", Icon: PhNotePencilDuotone },
+	INBOX: { name: "Inbox", Icon: PhNotePencilDuotone },
+	GALLERY: { name: "Gallery", Icon: PhNotePencilDuotone },
+	VAULT: { name: "Vault", Icon: PhNotePencilDuotone },
+	PASSWORDS: { name: "Passwords", Icon: PhNotePencilDuotone },
+	REPOSITORIES: { name: "Repositories", Icon: PhNotePencilDuotone },
+	CHAT: { name: "Chat", Icon: PhNotePencilDuotone }
 }
