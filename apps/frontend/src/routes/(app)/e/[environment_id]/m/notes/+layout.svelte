9956ef7b7b74d193be4ddc4973e91db313977e77
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2026-04-24 23:50:48 +0200

    wip: notes module

---
+++ apps/frontend/src/routes/(app)/e/[environment_id]/m/notes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Sidebar from "modules/notes/components/sidebar.svelte";
+
+	let { children } = $props();
+</script>
+
+<div class="flex h-full w-full">
+	<Sidebar />
+	{@render children()}
+</div>
