c1011d982f3f03513342de1b5646b7f15fa2c325
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2026-05-01 01:51:16 +0200

    fix: settings overlay

--- apps/frontend/src/ui/primitives/dialog/content.svelte
+++ apps/frontend/src/ui/primitives/dialog/content.svelte
@@ -20,7 +20,7 @@
 				in:presence={{ duration: 150, scale: 0.95, blur: 2 }}
 				out:presence={{ duration: 75, scale: 0.98, blur: 2 }}
 				class={cn(
-					"fixed left-1/2 top-1/2 -translate-1/2 border border-main-800 bg-main-950 shadow-2xl shadow-main-950 z-9999",
+					"fixed left-1/2 top-1/2 -translate-1/2 border border-main-800 bg-main-950 z-9999",
 					className,
 				)}
 			>
